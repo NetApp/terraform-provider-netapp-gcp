5f2a5962509185660688fa8390061cd2ac197020
Chris Archibald <carchi8py@gmail.com>
2020-09-01 20:10:02 -0400

    Sync bitbucket and Github

---
+++ gcp/data_source_gcp_active_directory.go
@@ -0,0 +1,96 @@
+package gcp
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceGCPActiveDirectory() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceGCPActiveDirectoryRead,
+		Schema: map[string]*schema.Schema{
+			"username": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"password": {
+				Type:      schema.TypeString,
+				Optional:  true,
+				Sensitive: true,
+			},
+			"domain": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"dns_server": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"net_bios": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"organizational_unit": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"site": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"region": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"uuid": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceGCPActiveDirectoryRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Client)
+	active_directory := listActiveDirectoryRequest{}
+	active_directory.Region = d.Get("region").(string)
+	var res listActiveDirectoryResult
+	res, err := client.listActiveDirectoryForRegion(active_directory)
+	if err != nil {
+		return err
+	}
+	d.SetId(res.UUID)
+
+	if err := d.Set("uuid", res.UUID); err != nil {
+		return fmt.Errorf("Error reading active directory UUID: %s", err)
+	}
+	if err := d.Set("domain", res.Domain); err != nil {
+		return fmt.Errorf("Error reading active directory domain: %s", err)
+	}
+
+	if err := d.Set("net_bios", res.NetBIOS); err != nil {
+		return fmt.Errorf("Error reading active directory net_bios: %s", err)
+	}
+
+	if err := d.Set("organizational_unit", res.OrganizationalUnit); err != nil {
+		return fmt.Errorf("Error reading active directory organizational_unit: %s", err)
+	}
+
+	if err := d.Set("site", res.Site); err != nil {
+		return fmt.Errorf("Error reading active directory site: %s", err)
+	}
+
+	if err := d.Set("username", res.Username); err != nil {
+		return fmt.Errorf("Error reading active directory username: %s", err)
+	}
+
+	if err := d.Set("dns_server", res.DNS); err != nil {
+		return fmt.Errorf("Error reading active directory dns_server: %s", err)
+	}
+
+	if err := d.Set("region", res.Region); err != nil {
+		return fmt.Errorf("Error reading active directory region: %s", err)
+	}
+
+	return nil
+}
--- gcp/helper.go
+++ gcp/helper.go
@@ -1,6 +1,7 @@
 package gcp
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -17,9 +18,10 @@ func apiResponseChecker(statusCode int, response []byte, funcName string) error 
 	if statusCode >= 300 || statusCode < 200 {
 		log.Printf("%s request failed", funcName)
 		var error_response apiErrorResponse
+		response_content := bytes.NewBuffer(response).String()
 		if err := json.Unmarshal(response, &error_response); err != nil {
 			log.Printf("Failed to unmarshall error response from %s", funcName)
-			return err
+			return fmt.Errorf(response_content)
 		}
 		return fmt.Errorf("code: %d, message: %s", error_response.Code, error_response.Message)
 	}
